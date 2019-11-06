f6398d5ed3e7b0d436c602bbf4416766305e5562
Roi <roiprezl@gmail.com>
2019-11-06 09:24:04 +0100

    Adds User integration tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,17 +9,20 @@ include_once '../Models/User/UserDAO.php';
 final class UserTest extends TestCase
 {
     protected static $userDAO;
+    protected static $userTestDAO;
 
     public static function setUpBeforeClass(): void
     {
-        $_SESSION['env'] = 'test';
+        $_SESSION['env'] = 'dev';
         self::$userDAO = new UserDAO();
+
+        $_SESSION['env'] = 'test';
+        self::$userTestDAO = new UserDAO();
     }
 
     public static function tearDownAfterClass(): void
     {
-        self::$userDAO->truncateTable();
-        $_SESSION['env'] = 'dev';
+        self::$userTestDAO->truncateTable();
     }
 
     public function testCanBeCreated()
@@ -37,9 +40,9 @@ final class UserTest extends TestCase
         $user = new User('roi', 'roipass', '44661643J', 'roi', 'roiprez', 'roiprezl',
             'calle falsa 123', '666444666');
 
-        self::$userDAO->add($user);
+        self::$userTestDAO->add($user);
 
-        $userCreated = self::$userDAO->show("login", "roi");
+        $userCreated = self::$userTestDAO->show("login", "roi");
 
         $this->assertInstanceOf(User::class, $userCreated);
     }
@@ -49,13 +52,13 @@ final class UserTest extends TestCase
         $user = new User('roi', 'roipass', '44661643J', 'roi', 'roiprez', 'roiprezl',
             'calle falsa 123', '666444666');
 
-        self::$userDAO->add($user);
+        self::$userTestDAO->add($user);
 
         $user->setAddress("Calle Falsa 124");
 
-        self::$userDAO->edit($user);
+        self::$userTestDAO->edit($user);
 
-        $userCreated = self::$userDAO->show("login", "roi");
+        $userCreated = self::$userTestDAO->show("login", "roi");
 
         $this->assertEquals($userCreated->getAddress(), "Calle Falsa 124");
     }
@@ -65,18 +68,18 @@ final class UserTest extends TestCase
         $user = new User('roi', 'roipass', '44661643J', 'roi', 'roiprez', 'roiprezl',
             'calle falsa 123', '666444666');
 
-        self::$userDAO->add($user);
+        self::$userTestDAO->add($user);
 
-        self::$userDAO->delete("login", "roi");
+        self::$userTestDAO->delete("login", "roi");
 
-        $userCreated = self::$userDAO->show("login", "roi");
+        $userCreated = self::$userTestDAO->show("login", "roi");
 
         $this->assertEmpty($userCreated);
     }
 
     public function testCanShowNone()
     {
-        $userCreated = self::$userDAO->showAll("login", "roi");
+        $userCreated = self::$userTestDAO->showAll("login", "roi");
 
         $this->assertEmpty($userCreated);
     }
@@ -92,14 +95,101 @@ final class UserTest extends TestCase
         $user3 = new User('roi3', 'roipass', '44661643J', 'Roi', 'Perez', 'roiprezl',
             'calle falsa 123', '666444666');
 
-        self::$userDAO->add($user1);
-        self::$userDAO->add($user2);
-        self::$userDAO->add($user3);
+        self::$userTestDAO->add($user1);
+        self::$userTestDAO->add($user2);
+        self::$userTestDAO->add($user3);
 
-        $usersCreated = self::$userDAO->showAll("name", "Roi");
+        $usersCreated = self::$userTestDAO->showAll("name", "Roi");
 
         $this->assertTrue($usersCreated[0]->getLogin() == 'roi');
         $this->assertTrue($usersCreated[1]->getLogin() == 'roi2');
         $this->assertTrue($usersCreated[2]->getLogin() == 'roi3');
+    }
+
+    public function testIntCanBeCreated()
+    {
+        $postData = array(
+            'submit' => true,
+            'login' => 'roi',
+            'password' => 'roi',
+            'dni' => 'roi',
+            'name' => 'roi',
+            'surname' => 'roi',
+            'email' => 'roi',
+            'address' => 'roi',
+            'telephone' => 'roi',
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/Controllers/UserController.php?action=add");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_exec($ch);
+        curl_close($ch);
+
+        $userCreated = self::$userDAO->show("login", "roi");
+
+        $this->assertInstanceOf(User::class, $userCreated);
+
+        self::$userDAO->delete('login', 'roi');
+    }
+
+    public function testIntCanBeUpdated()
+    {
+        $user = new User('roi', 'roipass', '44661643J', 'roi', 'roiprez', 'roiprezl',
+            'calle falsa 123', '666444666');
+
+        self::$userDAO->add($user);
+
+        $postData = array(
+            'submit' => true,
+            'login' => 'roi',
+            'password' => 'roi',
+            'dni' => 'roi',
+            'name' => 'roi',
+            'surname' => 'roi',
+            'email' => 'roi',
+            'address' => 'roi',
+            'telephone' => 'roi',
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/Controllers/UserController.php?action=edit&login=roi");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_exec($ch);
+        curl_close($ch);
+
+        $userCreated = self::$userDAO->show("login", "roi");
+
+        $this->assertEquals('roi', $userCreated->getAddress());
+
+        self::$userDAO->delete('login', 'roi');
+    }
+
+    public function testIntCanBeDeleted()
+    {
+        $user = new User('roi', 'roipass', '44661643J', 'roi', 'roiprez', 'roiprezl',
+            'calle falsa 123', '666444666');
+
+        self::$userDAO->add($user);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/Controllers/UserController.php?action=delete&login=roi");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_exec($ch);
+        curl_close($ch);
+
+        $userCreated = self::$userDAO->show("login", "roi");
+
+        $this->assertEmpty($userCreated);
     }
 }
