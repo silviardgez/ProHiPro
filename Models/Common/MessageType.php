abd2c44331d4ac34c518543ade798ecbe703c62f
Silvia Rodríguez <silviardguez@gmail.com>
2019-11-10 11:17:44 +0100

    Adds message toast translation
    This commit also allows to translate message shown in toasts.

--- Models/Common/MessageType.php
+++ Models/Common/MessageType.php
@@ -2,8 +2,8 @@
 
 class MessageType
 {
-    const INFO = ["INFO", "#F8F9FA"];
+    const INFO = ["INFORMACIÓN", "#F8F9FA"];
     const ERROR = ["ERROR", "#FB4242"];
-    const SUCCESS = ["SUCCESS", "#45D265"];
-    const WARNING = ["WARNING", "#FFFF7F"];
+    const SUCCESS = ["ÉXITO", "#45D265"];
+    const WARNING = ["ADVERTENCIA", "#FFFF7F"];
 }
--- Views/Common/MessageToast.php
+++ Views/Common/MessageToast.php
@@ -1,15 +1,14 @@
 <head><link rel="stylesheet" href="../CSS/toast.css" /></head>
-  <div class="toast-container">
+<div class="toast-container">
     <div class="toast" id="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="5000">
-      <div id="header-toast" class="toast-header">
-        <strong class="mr-auto" id="title-name"></strong>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="toast-body">
-          <p id="message-toast"></p>
-      </div>
+        <div id="header-toast" class="toast-header">
+            <button id="button-header" type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div id="toast-body" class="toast-body">
+        </div>
     </div>
-  </div>
+</div>
+
 
