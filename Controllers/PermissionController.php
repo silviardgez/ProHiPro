4a4b48c15f2133daaded84d5a12d7fa9c3a7f2f4
fctato <fercamta@gmail.com>
2019-11-11 01:36:00 +0100

    Adds ACL
    Adds ACL to PermissionController.
    Adds new DB creation script to made possible using it by the end-user.

--- Controllers/PermissionController.php
+++ Controllers/PermissionController.php
@@ -2,6 +2,7 @@
 
 session_start();
 include '../Functions/Authentication.php';
+include '../Functions/HavePermission.php';
 
 if (!IsAuthenticated()){
     header('Location:../index.php');
@@ -25,92 +26,8 @@ include '../Functions/Redirect.php';
 $permission = isset($_REQUEST['action']) ? $_REQUEST['action'] : "";
 switch($permission) {
     case "add":
-        if (!isset($_POST["submit"])){
-            $actionDAO = new ActionDAO();
-            $funcDAO = new FunctionalityDAO();
-            $actionsData = $actionDAO->showAll();
-            $functionalitiesData = $funcDAO->showAll();
-            $funcActionDAO = new FuncActionDAO();
-            $funcActionData = $funcActionDAO->showAll();
-            $roleDAO = new RoleDAO();
-            $roleData = $roleDAO->showAll();
-            new PermissionAddView($roleData,$funcActionData, $actionsData,$functionalitiesData);
-        } else {
-            try {
-                $permission = new Permission();
-                $permission->setIdRole($_POST["idRole"]);
-                $permission->setIdFuncAction($_POST["idFuncAction"]);
-                $permissionDAO = new PermissionDAO();
-
-                $allPermissions = $permissionDAO->showAll();
-                foreach ($allPermissions as $permis) {
-                    if ($permis->getIdRole() == $permission->getIdRole() and $permis->getIdFuncAction() == $permission->getIdFuncAction()) {
-                        throw new DAOException('Permiso duplicado.');
-                    }
-                }
-                $permissionDAO->add($permission);
-                $message = MessageType::SUCCESS;
-                showAll();
-                showToast($message, "Permiso añadido correctamente.");
-            } catch (DAOException $e) {
-                $message = MessageType::ERROR;
-                showAll();
-                showToast($message, $e->getMessage());
-            }
-        }
-        break;
-    case "delete":
-        if (isset($_REQUEST["confirm"])) {
-            try {
-                $key = "IdPermission";
-                $value = $_REQUEST[$key];
-                $permissionDAO = new PermissionDAO();
-                $response = $permissionDAO->delete($key, $value);
-                $message = MessageType::SUCCESS;
-                showAll();
-                showToast($message, "Permiso eliminado correctamente.");
-            } catch (DAOException $e) {
-                $message = MessageType::ERROR;
-                showAll();
-                showToast($message, $e->getMessage());
-            }
-        } else {
-            showAll();
-            $key = "IdPermission";
-            $value = $_REQUEST[$key];
-            openDeletionModal("Eliminar Permiso " . $value, "¿Está seguro de que desea eliminar " .
-                "el permiso <b>" . $value . "</b>? Esta acción es permanente y no se puede recuperar.",
-                "../Controllers/PermissionController.php?action=delete&IdPermission=" . $value . "&confirm=true");
-        }
-        break;
-    case "show":
-        try {
-            $key = "IdPermission";
-            $value = $_REQUEST[$key];
-            $permissionDAO = new PermissionDAO();
-            $funcActionDAO = new FuncActionDAO();
-            $funcActionData = $funcActionDAO->showAll();
-            $actionDAO = new ActionDAO();
-            $funcDAO = new FunctionalityDAO();
-            $actionsData = $actionDAO->showAll();
-            $functionalitiesData = $funcDAO->showAll();
-            $roleDAO = new RoleDAO();
-            $roleData = $roleDAO->showAll();
-            $permissionData = $permissionDAO->show($key, $value);
-            new PermissionShowView($permissionData, $roleData, $funcActionData, $actionsData, $functionalitiesData);
-        } catch (DAOException $e) {
-            $message = MessageType::ERROR;
-            showAll();
-            showToast($message, $e->getMessage());
-        }
-        break;
-    case "edit":
-        $key = "IdPermission";
-        $value = $_REQUEST[$key];
-        $permissionDAO = new PermissionDAO();
-        try {
-            $permission = $permissionDAO->show($key, $value);
-            if (!isset($_POST["submit"])){
+        if(HavePermission("Permission", "ADD")) {
+            if (!isset($_POST["submit"])) {
                 $actionDAO = new ActionDAO();
                 $funcDAO = new FunctionalityDAO();
                 $actionsData = $actionDAO->showAll();
@@ -119,13 +36,12 @@ switch($permission) {
                 $funcActionData = $funcActionDAO->showAll();
                 $roleDAO = new RoleDAO();
                 $roleData = $roleDAO->showAll();
-                new PermissionEditView($permission,$roleData, $funcActionData,$actionsData,$functionalitiesData);
+                new PermissionAddView($roleData, $funcActionData, $actionsData, $functionalitiesData);
             } else {
                 try {
-                    $permission->setIdPermission($value);
+                    $permission = new Permission();
                     $permission->setIdRole($_POST["idRole"]);
                     $permission->setIdFuncAction($_POST["idFuncAction"]);
-
                     $permissionDAO = new PermissionDAO();
 
                     $allPermissions = $permissionDAO->showAll();
@@ -134,21 +50,114 @@ switch($permission) {
                             throw new DAOException('Permiso duplicado.');
                         }
                     }
-
-                    $response = $permissionDAO->edit($permission);
+                    $permissionDAO->add($permission);
                     $message = MessageType::SUCCESS;
                     showAll();
-                    showToast($message, "Permiso editado correctamente.");
+                    showToast($message, "Permiso añadido correctamente.");
                 } catch (DAOException $e) {
                     $message = MessageType::ERROR;
                     showAll();
                     showToast($message, $e->getMessage());
                 }
             }
-        } catch (DAOException $e) {
-            $message = MessageType::ERROR;
-            showAll();
-            showToast($message, $e->getMessage());
+        }
+        break;
+    case "delete":
+        if(HavePermission("Permission", "DELETE")) {
+            if (isset($_REQUEST["confirm"])) {
+                try {
+                    $key = "IdPermission";
+                    $value = $_REQUEST[$key];
+                    $permissionDAO = new PermissionDAO();
+                    $response = $permissionDAO->delete($key, $value);
+                    $message = MessageType::SUCCESS;
+                    showAll();
+                    showToast($message, "Permiso eliminado correctamente.");
+                } catch (DAOException $e) {
+                    $message = MessageType::ERROR;
+                    showAll();
+                    showToast($message, $e->getMessage());
+                }
+            } else {
+                showAll();
+                $key = "IdPermission";
+                $value = $_REQUEST[$key];
+                openDeletionModal("Eliminar Permiso " . $value, "¿Está seguro de que desea eliminar " .
+                    "el permiso <b>" . $value . "</b>? Esta acción es permanente y no se puede recuperar.",
+                    "../Controllers/PermissionController.php?action=delete&IdPermission=" . $value . "&confirm=true");
+            }
+        }
+        break;
+    case "show":
+        if(HavePermission("Permission", "SHOWCURRENT")) {
+            try {
+                $key = "IdPermission";
+                $value = $_REQUEST[$key];
+                $permissionDAO = new PermissionDAO();
+                $funcActionDAO = new FuncActionDAO();
+                $funcActionData = $funcActionDAO->showAll();
+                $actionDAO = new ActionDAO();
+                $funcDAO = new FunctionalityDAO();
+                $actionsData = $actionDAO->showAll();
+                $functionalitiesData = $funcDAO->showAll();
+                $roleDAO = new RoleDAO();
+                $roleData = $roleDAO->showAll();
+                $permissionData = $permissionDAO->show($key, $value);
+                new PermissionShowView($permissionData, $roleData, $funcActionData, $actionsData, $functionalitiesData);
+            } catch (DAOException $e) {
+                $message = MessageType::ERROR;
+                showAll();
+                showToast($message, $e->getMessage());
+            }
+        }
+        break;
+    case "edit":
+        if(HavePermission("Permission", "EDIT")) {
+            $key = "IdPermission";
+            $value = $_REQUEST[$key];
+            $permissionDAO = new PermissionDAO();
+            try {
+                $permission = $permissionDAO->show($key, $value);
+                if (!isset($_POST["submit"])) {
+                    $actionDAO = new ActionDAO();
+                    $funcDAO = new FunctionalityDAO();
+                    $actionsData = $actionDAO->showAll();
+                    $functionalitiesData = $funcDAO->showAll();
+                    $funcActionDAO = new FuncActionDAO();
+                    $funcActionData = $funcActionDAO->showAll();
+                    $roleDAO = new RoleDAO();
+                    $roleData = $roleDAO->showAll();
+                    new PermissionEditView($permission, $roleData, $funcActionData, $actionsData, $functionalitiesData);
+                } else {
+                    try {
+                        $permission->setIdPermission($value);
+                        $permission->setIdRole($_POST["idRole"]);
+                        $permission->setIdFuncAction($_POST["idFuncAction"]);
+
+                        $permissionDAO = new PermissionDAO();
+
+                        $allPermissions = $permissionDAO->showAll();
+                        foreach ($allPermissions as $permis) {
+                            if ($permis->getIdRole() == $permission->getIdRole() and $permis->getIdFuncAction() == $permission->getIdFuncAction()) {
+                                throw new DAOException('Permiso duplicado.');
+                            }
+                        }
+
+                        $response = $permissionDAO->edit($permission);
+                        $message = MessageType::SUCCESS;
+                        showAll();
+                        showToast($message, "Permiso editado correctamente.");
+                    } catch (DAOException $e) {
+                        $message = MessageType::ERROR;
+                        showAll();
+                        showToast($message, $e->getMessage());
+                    }
+                }
+            } catch (DAOException $e) {
+                $message = MessageType::ERROR;
+                showAll();
+                showToast($message, $e->getMessage());
+            }
         }
         break;
     default:
@@ -157,21 +166,28 @@ switch($permission) {
 }
 
 function showAll() {
-    try {
-        $funcActionDAO = new FuncActionDAO();
-        $actionDAO = new ActionDAO();
-        $funcDAO = new FunctionalityDAO();
-        $funcActionsData = $funcActionDAO->showAll();
-        $actionsData = $actionDAO->showAll();
-        $functionalitiesData = $funcDAO->showAll();
-        $permissionDAO = new PermissionDAO();
-        $permissionData = $permissionDAO->showAll();
-        $roleDAO = new RoleDAO();
-        $roleData = $roleDAO->showAll();
-        new PermissionShowAllView($permissionData, $roleData, $funcActionsData, $actionsData, $functionalitiesData);
-    } catch (DAOException $e) {
+    if(HavePermission("Permission", "SHOWALL")) {
+        try {
+            $funcActionDAO = new FuncActionDAO();
+            $actionDAO = new ActionDAO();
+            $funcDAO = new FunctionalityDAO();
+            $funcActionsData = $funcActionDAO->showAll();
+            $actionsData = $actionDAO->showAll();
+            $functionalitiesData = $funcDAO->showAll();
+            $permissionDAO = new PermissionDAO();
+            $permissionData = $permissionDAO->showAll();
+            $roleDAO = new RoleDAO();
+            $roleData = $roleDAO->showAll();
+            new PermissionShowAllView($permissionData, $roleData, $funcActionsData, $actionsData, $functionalitiesData);
+        } catch (DAOException $e) {
+            $message = MessageType::ERROR;
+            new UserRoleShowAllView(array());
+            showToast($message, $e->getMessage());
+        }
+    } else{
         $message = MessageType::ERROR;
-        new UserRoleShowAllView(array());
-        showToast($message, $e->getMessage());
+        showToast($message,"Access Denied");
+//        redirect("./IndexController.php");
+
     }
 }
