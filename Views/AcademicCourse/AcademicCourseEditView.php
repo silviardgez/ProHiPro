b3172befd048752c3562d3f3d42f1b542fc1c134
fctato <fercamta@gmail.com>
2019-11-15 21:57:00 +0100

    Adds validations to Academic Course
    Validations and translations in Academic Course

--- Views/AcademicCourse/AcademicCourseEditView.php
+++ Views/AcademicCourse/AcademicCourseEditView.php
@@ -10,7 +10,7 @@ class AcademicCourseEditView {
         <head>
             <link rel="stylesheet" href="../CSS/default.css" />
             <link rel="stylesheet" href="../CSS/forms.css" />
-            <script src="../Validations.js"></script>
+            <script src="../JS/Validations/AcademicCourseValidations.js"></script>
         </head>
         <main role="main" class="margin-main ml-sm-auto px-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3
@@ -18,22 +18,22 @@ class AcademicCourseEditView {
                 <h1 class="h2" data-translate="Curso académico %<?php echo $this->academicCourse->getAcademicCourseAbbr() ?>%"></h1>
                 <a class="btn btn-primary" role="button" href="../Controllers/AcademicCourseController.php"><p data-translate="Volver"></p></a>
             </div>
-            <form id="academicCourseForm" name = "EDIT" action='../Controllers/AcademicCourseController.php?action=edit' method='POST' onsubmit="return checkEditAcademicCourse()">
+            <form id="academicCourseEditForm" name = "EDIT" action='../Controllers/AcademicCourseController.php?action=edit' method='POST' onsubmit="return areAcademicCourseEditFieldsCorrect()">
                 <div class="form-group">
                     <label for="IdAcademicCourse" data-translate="Identificador"></label>
                     <input type="hidden" name="id" value="<?php echo $this->academicCourse->getId() ?>" >
                     <input type="text" class="form-control" id="academicCourseAbbr" name="academicCourseAbbr"
                            value="<?php echo $this->academicCourse->getAcademicCourseAbbr() ?>" readonly>
                 </div>
-                <div class="form-group">
+                <div id="start-year-div" class="form-group">
                     <label for="start_year" data-translate="Año de inicio"></label>
                     <input type="number" min="2000" max="9999" class="form-control" id="start_year" name="start_year"
-                           value="<?php echo $this->academicCourse->getStartYear() ?>" required onblur="checkEmpty(this)">
+                           value="<?php echo $this->academicCourse->getStartYear() ?>" oninput="checkStartYearEmptyAcademicCourse(this)" required >
                 </div>
-                <div class="form-group">
+                <div id="end-year-div" class="form-group">
                     <label for="end_year" data-translate="Año de fin"></label>
                     <input type="number" min="2000" max="9999" class="form-control" id="end_year" name="end_year"
-                           value="<?php echo $this->academicCourse->getEndYear() ?>" required onblur="checkEmpty(this)">
+                           value="<?php echo $this->academicCourse->getEndYear() ?>" oninput="checkEndYearEmptyAcademicCourse(this)" required >
                 </div>
 
                 <button name="submit" type="submit" class="btn btn-primary" data-translate="Enviar"></button>
