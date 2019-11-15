eb8ab4bdc7468bbe86c5ea4904465df94f856fe9
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-11-15 20:28:12 +0100

    Adds role, action and functionality front-end validations
    This commit adapts the front-end validations of this entities to use
    the previous refactorizations.

---
+++ JS/Validations/ActionValidations.js
@@ -0,0 +1,58 @@
+function checkNameAction(field) {
+    const name = "nombre";
+    if ((toret = checkEmpty(field, name)) === "" && (toret = checkLength(field,'60', name)) === "" 
+        && (toret = checkText(field,'60', name)) === "" && (toret = checkAlphabetical(field,'60', name)) === "") {
+        deleteMessage(name);
+        return true;
+    } else {
+        showMessage('name-div', name, toret, field);
+        return false;
+    }
+}
+
+function checkNameEmptyAction(field) {
+    if (field.value.length > 0) {
+        return checkNameAction(field);
+    } else {
+        return true;
+    }
+}
+
+function checkDescriptionAction(field) {
+    const name = "descripción";
+	const nameDiv = "description";
+    if ((toret = checkEmpty(field, name)) === "" && (toret = checkLength(field,'100', name)) === "" 
+        && (toret = checkText(field,'100', name)) === "") {
+        deleteMessage(nameDiv);
+        return true;
+    } else {
+        showMessage('description-div', nameDiv, toret, field);
+        return false;
+    }
+}
+
+function checkDescriptionEmptyAction(field) {
+    if (field.value.length > 0) {
+        return checkDescriptionAction(field);
+    } else {
+        return true;
+    }
+}
+
+function areUserFieldsCorrect() {
+    form = $('#actionForm')[0];
+    if(checkNameAction(form.elements[0]) && checkDescriptionAction(form.elements[1])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function areActionSearchFieldsCorrect() {
+    form = $('#actionSearchForm')[0];
+    if(checkNameEmptyAction(form.elements[0]) && checkDescriptionEmptyAction(form.elements[1])) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- Views/Action/ActionSearchView.php
+++ Views/Action/ActionSearchView.php
@@ -9,20 +9,24 @@ class ActionSearchView {
         <head>
             <link rel="stylesheet" href="../CSS/default.css" />
             <link rel="stylesheet" href="../CSS/forms.css" />
+			<script src="../JS/Validations/ActionValidations.js"></script>
         </head>
         <main role="main" class="margin-main ml-sm-auto px-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
                 <h1 class="h2" data-translate="Búsqueda de acciones"></h1>
                 <a class="btn btn-primary" role="button" href="../Controllers/ActionController.php" data-translate="Volver"></a>
             </div>
-            <form id="actionForm" action='../Controllers/ActionController.php?action=search' method='POST'>
-                <div class="form-group">
+            <form id="actionSearchForm" action='../Controllers/ActionController.php?action=search' method='POST' 
+			onsubmit="return areActionSearchFieldsCorrect()">
+                <div id="name-div" class="form-group">
                     <label for="name" data-translate="Nombre"></label>
-                    <input type="text" class="form-control" id="name" name="name" data-translate="Introducir nombre">
+                    <input type="text" class="form-control" id="name" name="name" data-translate="Introducir nombre" 
+					max-length="60" oninput="checkNameEmptyAction(this);">
                 </div>
-                <div class="form-group">
+                <div id="description-div" id="description-div"  class="form-group">
                     <label for="description" data-translate="Descripción"></label>
-                    <input type="text" class="form-control" id="description" name="description" data-translate="Introducir descripción">
+                    <input type="text" class="form-control" id="description" name="description" data-translate="Introducir descripción" 
+					max-length="100" oninput="checkDescriptionEmptyAction(this);">
                 </div>
                 <button name="submit" type="submit" class="btn btn-primary" data-translate="Enviar"></button>
             </form>
