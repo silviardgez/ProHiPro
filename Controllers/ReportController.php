2e1e986d4d994e6c2f6a5799176f8ea736ab2775
Pedro Celard <pedrocelard@gmail.com>
2019-12-18 08:47:53 +0100

    Crea csv en una carpeta temporal
    Modificando los permisos en la carpeta para poder crearla, pero sigue sin enviarla al cliente.

--- Controllers/ReportController.php
+++ Controllers/ReportController.php
@@ -34,11 +34,14 @@ $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : "";
 switch ($action) {
     case "downloadCSV":
         try{
+            $dir_subida = '/var/www/html/temp/';
             array_to_csv_download(array(
                 array(1,2,3,4), // this array is going to be the first row
                 array(1,2,3,4)), // this array is going to be the second row
                 "numbers.csv"
             );
+
+            rrmdir($dir_subida);
         }catch (DAOException $e) {
             goToShowAllAndShowError($e->getMessage());
         } catch (ValidationException $ve) {
@@ -124,21 +127,55 @@ function goToShowAllAndShowSuccess($message) {
 }
 
 function array_to_csv_download($array, $filename = "export.csv", $delimiter=";") {
+
+    $dir_subida = '/var/www/html/temp/';
+
+    if (!file_exists($dir_subida)) {
+        mkdir($dir_subida, 0777, true);
+    }
+
     // open raw memory as file so no temp files needed, you might run out of memory though
-    $f = fopen('php://memory', 'w');
+    $f = fopen('/var/www/html/temp/temp.csv', 'w');
+
     // loop over the input array
     foreach ($array as $line) {
         // generate csv lines from the inner arrays
         fputcsv($f, $line, $delimiter);
     }
+
+    fclose($f);
+
+    if( !file_exists('/var/www/html/temp/temp.csv') ) die("File not found");
+
+
+    $f = fopen('/var/www/html/temp/temp.csv', 'w');
     // reset the file pointer to the start of the file
     fseek($f, 0);
+    header("Cache-Control: no-cache, must-revalidate");
     // tell the browser it's going to be a csv file
-    header('Content-Type: application/csv');
+    header('Content-Type: application/octet-stream;');
     // tell the browser we want to save it instead of displaying it
-    header('Content-Disposition: attachment; filename="'.$filename.'";');
+    header('Content-Disposition: attachment; filename="temp.csv";');
+    header("Content-Length: " . filesize($f));
+    header('Pragma: public');
     // make php send the generated csv lines to the browser
-    echo fpassthru($f);
-    fclose($f);
+
+    fpassthru($f);
     exit;
+}
+
+function rrmdir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir . "/" . $object) && !is_link($dir . "/" . $object))
+                    rrmdir($dir . "/" . $object);
+                else
+                    unlink($dir . "/" . $object);
+            }
+        }
+        rmdir($dir);
+    }
 }
