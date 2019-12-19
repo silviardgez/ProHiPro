7ae29c1a6b0832ead4a105aa1cc6090c88123d17
fctato <fercamta@gmail.com>
2019-12-19 20:09:04 +0100

    Descarga csv actualized

--- Functions/ExportCSV.php
+++ Functions/ExportCSV.php
@@ -10,9 +10,18 @@ function array_to_csv_download($array, $filename = "export.csv", $delimiter=";")
     // open raw memory as file so no temp files needed, you might run out of memory though
     $f = fopen('php://output', 'w');
     // loop over the input array
+    $entity = get_class($array[0]);
+    $first = true;
     foreach ($array as $line) {
+        if ($entity=="Center"){
+            if ($first){
+                $set=["Id","Name","University","Building"];
+                fputcsv($f, $set, $delimiter);
+            }
+            $set = [$line->getId(),$line->getName(),$line->getUniversity(),$line->getBuilding()];
+        }
         // generate csv lines from the inner arrays
-        $set=[$line->getName(),$line->getSurname()];
+//        $set=[$line->getName(),$line->getSurname()];
         fputcsv($f, $set, $delimiter);
     }
     // tell the browser it's going to be a csv file
