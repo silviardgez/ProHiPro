36c24f7b60cd8e12beddf3d3a05c9c204646f58e
Roi <roiprezl@gmail.com>
2019-12-19 20:56:26 +0100

    Adds more cositas

--- Controllers/ReportController.php
+++ Controllers/ReportController.php
@@ -8,10 +8,13 @@ if (!IsAuthenticated()) {
 
 include_once '../Models/User/UserDAO.php';
 include_once '../Models/Common/DAOException.php';
+include_once '../Models/University/UniversityDAO.php';
 include_once '../Views/Common/Head.php';
 include_once '../Views/Common/DefaultView.php';
 include_once '../Views/Report/ReportShowAllView.php';
 include_once '../Views/Report/ReportSearchView.php';
+include_once '../Views/Report/ReportCenterSearchView.php';
+include_once '../Views/Report/ReportCenterShowAllView.php';
 include_once '../Views/User/UserShowAllView.php';
 include_once '../Views/User/UserAddView.php';
 include_once '../Views/User/UserShowView.php';
@@ -26,42 +29,50 @@ include_once '../Functions/Pagination.php';
 include_once '../Functions/GetReportResults.php';
 
 
+//DAOS
+$universityDAO = new UniversityDAO();
+
+//Data required
+$universityData = $universityDAO->showAll();
+
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : "";
 switch ($action) {
     case "search":
-        //TODO: Cambiar el permiso a User
-        if (HavePermission("User", "SEARCH")) {
-            if (!isset($_POST["entity"])) {
-                new ReportSearchView();
-            } else {
-                switch($_POST["entity"]) {
-                    case "center":
-                        if (!isset($_POST["submit"])) {
-                            new ReportCenterSearchView();
-                        } else {
-                            try {
-                                $sql = "SELECT CENTER.* FROM CENTER";
-                                $universityId = $_POST["universityId"];
-                                if($universityId != NULL){
-                                    $sql .= ", UNIVERSITY WHERE UNIVERSITY.id =" . $universityId.";";
-                                }
-                                echo $sql;
-                                $reportDump = returnData($sql);
-                                echo $reportDump;
-                                new ReportCenterShowAllView($reportDump);
-                                goToShowAllAndShowSuccess("Departamento añadido correctamente.");
-                            } catch (DAOException $e) {
-                                goToShowAllAndShowError($e->getMessage());
-                            } catch (ValidationException $ve) {
-                                goToShowAllAndShowError($ve->getMessage());
-                            }
-                        }
-                        break;
-                }
-            }
+        if (!isset($_POST["entity"])) {
+            new ReportSearchView();
         } else {
-            goToShowAllAndShowError("No tienes permiso para buscar.");
+            echo($_POST['entity']);
+            switch($_POST["entity"]) {
+                case "center":
+                    if (!isset($_POST["university"])) {
+                        new ReportCenterSearchView($universityData);
+                    } else {
+                        try {
+                            $sql = "SELECT CENTER.* FROM CENTER";
+                            $universityId = $_POST["universityId"];
+                            print_r($universityId);
+                            if($universityId != NULL){
+                                $sql = $sql . ", UNIVERSITY WHERE UNIVERSITY.id =" . $universityId . ";";
+                            }
+                            print_r($sql);
+                            $reportDump = returnData($sql);
+                            print_r($reportDump);
+                            new ReportCenterShowAllView($reportDump);
+                            goToShowAllAndShowSuccess("Departamento añadido correctamente.");
+                        } catch (DAOException $e) {
+                            goToShowAllAndShowError($e->getMessage());
+                        } catch (ValidationException $ve) {
+                            goToShowAllAndShowError($ve->getMessage());
+                        }
+                    }
+                    break;
+
+                default:
+                    showAll();
+                    break;
+            }
         }
+    
         break;
     default:
         showAll();
