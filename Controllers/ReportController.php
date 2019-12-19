5e4fdf155cd3c160c59146aa12a33afafb3984a2
Roi <roiprezl@gmail.com>
2019-12-19 20:14:25 +0100

    Adds report generation to controller

--- Controllers/ReportController.php
+++ Controllers/ReportController.php
@@ -23,48 +23,40 @@ include_once '../Functions/OpenDeletionModal.php';
 include_once '../Functions/Redirect.php';
 include_once '../Functions/Messages.php';
 include_once '../Functions/Pagination.php';
+include_once '../Functions/GetReportResults.php';
 
-//DAO
-$userDAO = new UserDAO();
-
-$userPrimaryKey = "login";
-$value = $_REQUEST[$userPrimaryKey];
 
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : "";
 switch ($action) {
     case "search":
-        if (HavePermission("User", "SHOWALL")) {
-            if (!isset($_POST["submit"])) {
+        //TODO: Cambiar el permiso a User
+        if (HavePermission("User", "SEARCH")) {
+            if (!isset($_POST["entity"])) {
                 new ReportSearchView();
             } else {
-                try {
-                    $user = new User();
-                    if(!empty($_POST["login"])) {
-                        $user->setLogin($_POST["login"]);
-                    }
-                    if(!empty($_POST["dni"])) {
-                        $user->setDni($_POST["dni"]);
-                    }
-                    if(!empty($_POST["name"])) {
-                        $user->setName($_POST["name"]);
-                    }
-                    if(!empty($_POST["surname"])) {
-                        $user->setSurname($_POST["surname"]);
-                    }
-                    if(!empty($_POST["email"])) {
-                        $user->setEmail($_POST["email"]);
-                    }
-                    if(!empty($_POST["address"])) {
-                        $user->setAddress($_POST["address"]);
-                    }
-                    if(!empty($_POST["telephone"])) {
-                        $user->setTelephone($_POST["telephone"]);
-                    }
-                    showAllSearch($user);
-                } catch (DAOException $e) {
-                    goToShowAllAndShowError($e->getMessage());
-                } catch (ValidationException $ve) {
-                    goToShowAllAndShowError($ve->getMessage());
+                switch($_POST["entity"]) {
+                    case "center":
+                        if (!isset($_POST["submit"])) {
+                            new ReportCenterSearchView();
+                        } else {
+                            try {
+                                $sql = "SELECT CENTER.* FROM CENTER";
+                                $universityId = $_POST["universityId"];
+                                if($universityId != NULL){
+                                    $sql .= ", UNIVERSITY WHERE UNIVERSITY.id =" . $universityId.";";
+                                }
+                                echo $sql;
+                                $reportDump = returnData($sql);
+                                echo $reportDump;
+                                new ReportCenterShowAllView($reportDump);
+                                goToShowAllAndShowSuccess("Departamento añadido correctamente.");
+                            } catch (DAOException $e) {
+                                goToShowAllAndShowError($e->getMessage());
+                            } catch (ValidationException $ve) {
+                                goToShowAllAndShowError($ve->getMessage());
+                            }
+                        }
+                        break;
                 }
             }
         } else {
@@ -78,35 +70,6 @@ switch ($action) {
 
 function showAll()
 {
-    showAllSearch(NULL);
+    new ReportSearchView();
 }
-
-function showAllSearch($search) {
-    if (HavePermission("User", "SHOWALL")) {
-        try {
-            $currentPage = getCurrentPage();
-            $itemsPerPage = getItemsPerPage();
-            $toSearch = getToSearch($search);
-            $usersData = $GLOBALS["userDAO"]->showAllPaged($currentPage, $itemsPerPage, $toSearch);
-            $totalUsers = $GLOBALS["userDAO"]->countTotalUsers($toSearch);
-            new ReportShowAllView($usersData, $itemsPerPage, $currentPage, $totalUsers, $toSearch);
-        } catch (DAOException $e) {
-            new ReportShowAllView(array());
-            errorMessage($e->getMessage());
-        }
-    } else {
-        accessDenied();
-    }
-}
-
-function goToShowAllAndShowError($message) {
-    showAll();
-    errorMessage($message);
-}
-
-function goToShowAllAndShowSuccess($message) {
-    showAll();
-    successMessage($message);
-}
-
 
