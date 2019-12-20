1f4efc530d8358351674df106ce440589a21be31
Roi <roiprezl@gmail.com>
2019-12-20 11:08:42 +0100

    Adds report test

--- Tests/ReportTest.php
+++ Tests/ReportTest.php
@@ -15,7 +15,6 @@ include_once '../Models/User/UserDAO.php';
 include_once '../Models/Building/BuildingDAO.php';
 include_once '../Models/Common/DAOException.php';
 include_once './testDB.php';
-include_once '../Functions/ExportCSV.php';
 
 final class ReportTest extends TestCase
 {
@@ -41,7 +40,7 @@ final class ReportTest extends TestCase
         $buildingDAO->add($building);
         $centerDAO->add(new Center(1, $university, 'ESEI', $building, $user));
         $centerDAO->add(new Center(2, $university, 'FF.CC. Ciencias ambientales', $building, $user));
-        $centerDAO->add(new Center(3, $university, 'FF.CC. Ingeniería de los alimentos', $building, $user));
+        $centerDAO->add(new Center(3, $university, 'FF.CC. Ing. de los alimentos', $building, $user));
         self::$array = $centerDAO->showAll();
     }
 
@@ -53,10 +52,55 @@ final class ReportTest extends TestCase
         }
     }
 
-
-    public function testCanCreateSubjectTeachers()
+    private function array_to_csv_download($array, $filename = "export.csv", $delimiter = ";")
     {
-        print_r(self::$array);
-        array_to_csv_download(self::$array, "export.csv");
+        $f = fopen('./files/sample.csv', 'w');
+        // loop over the input array
+        $entity = get_class($array[0]);
+        $first = true;
+        foreach ($array as $line) {
+            if ($entity == "Center") {
+                if ($first) {
+                    $set = ["Id", "Name", "University", "Building"];
+                    fputcsv($f, $set, $delimiter);
+                    $first = false;
+                }
+                $set = [$line->getId(), $line->getName(), $line->getUniversity()->getName(), $line->getBuilding()->getName()];
+            } elseif ($entity == "Degree") {
+                if ($first) {
+                    $set = ["Id", "Name", "Center", "Capacity", "Description", "Credits"];
+                    fputcsv($f, $set, $delimiter);
+                    $first = false;
+                }
+                $set = [$line->getId(), $line->getName(), $line->getCenter()->getName(), $line->getCapacity(), $line->getDescription(), $line->getCredits()];
+            }
+            // generate csv lines from the inner arrays
+//        $set=[$line->getName(),$line->getSurname()];
+            fputcsv($f, $set, $delimiter);
+        }
+        // tell the browser it's going to be a csv file
+        header('Content-Type: application/csv');
+        // tell the browser we want to save it instead of displaying it
+        header('Content-Disposition: attachment; filename="' . $entity . "sReport.csv" . '";');
+        fclose($f);
+    }
+
+
+    public function testCanCreateCSV()
+    {
+        self::array_to_csv_download(self::$array, "export.csv");
+        $csv_f = fopen('./files/sample.csv', 'r');
+        $lines = array();
+
+        $lines[0] = fgets($csv_f);
+        $lines[1] = fgets($csv_f);
+        $lines[2] = fgets($csv_f);
+        $lines[3] = fgets($csv_f);
+
+        $this->assertEquals(preg_split("/;/", $lines[1])[1], 'ESEI');
+        $this->assertEquals(preg_split("/;/", $lines[2])[1], '"FF.CC. Ciencias ambientales"');
+        $this->assertEquals(preg_split("/;/", $lines[3])[1], '"FF.CC. Ing. de los alimentos"');
+
+        fclose($csv_f);
     }
 }
